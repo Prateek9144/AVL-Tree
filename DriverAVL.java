8843d2c7df51d720a5f91768ed6082971d2fb947
Prateek Chouhan <42679687+Prateek9144@users.noreply.github.com>
2019-05-11 15:20:01 +0530

    Update DriverAVL.java

--- DriverAVL.java
+++ DriverAVL.java
@@ -6,12 +6,20 @@ class DriverAVL {
         obj.insert(20);
         obj.insert(101);
         obj.insert(75);
-        obj.insert(60);
         obj.insert(150);
         obj.insert(60);
         obj.insert(99);
         obj.insert(132);
+        System.out.println("Inorder Traversal");
         obj.infixTraverse(obj.root);
+        System.out.println("\nRoot : " + obj.root.val);
+        obj.Delete(132);
+        obj.Delete(30);
+        obj.Delete(75);
+        obj.Delete(60);
+        System.out.println("Inorder Traversal");
+        obj.infixTraverse(obj.root);
+        System.out.println("\nRoot : " + obj.root.val);
 
     }
 }
